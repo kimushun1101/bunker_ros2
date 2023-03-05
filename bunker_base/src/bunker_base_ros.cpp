76d7b613db5c0caa98a8fb0ab4f0d24c796a2e73
Luca Bascetta <luca.bascetta@polimi.it>
2023-03-05 11:41:48 +0100

    Changes to solve the error: no matching function for call to ‘westonrobot::BunkerBaseRos::declare_parameter(const char [10])’ compiling with ROS2 Humble (same as for scout_ros2 package)

--- bunker_base/src/bunker_base_ros.cpp
+++ bunker_base/src/bunker_base_ros.cpp
@@ -15,15 +15,15 @@
 namespace westonrobot {
 BunkerBaseRos::BunkerBaseRos(std::string node_name)
     : rclcpp::Node(node_name), keep_running_(false) {
-  this->declare_parameter("port_name");   
+  this->declare_parameter("port_name", "can0");
 
-  this->declare_parameter("odom_frame");
-  this->declare_parameter("base_frame");
-  this->declare_parameter("odom_topic_name");
+  this->declare_parameter("odom_frame", "odom");
+  this->declare_parameter("base_frame", "base_link");
+  this->declare_parameter("odom_topic_name", "odom");
 
-  this->declare_parameter("is_bunker_mini");
-  this->declare_parameter("simulated_robot");
-  this->declare_parameter("control_rate");
+  this->declare_parameter("is_bunker_mini", "false");
+  this->declare_parameter("simulated_robot", "false");
+  this->declare_parameter("control_rate", "50");
 
   LoadParameters();
 }
